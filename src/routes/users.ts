6fbba3dae0d0f86f1945e5d9cf8e3801128be363
Elevattd <marianoo.14.md@gmail.com>
2023-06-20 03:27:26 -0300

    sumo a las rutas los middlewares y las funciones encargadas de cada ruta

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { getUsers } from '../services/users/getUsers';
+import { apiKeyMiddleware } from '../middlewares/apiKeyMiddleware';
+import { barerTokenMiddleware } from '../middlewares/barerTokenMiddleware';
+import { createUser } from '../services/users/createUsers';
 
 export const router = Router();
 
-router.get('/', (req, res) => {
-	console.log('hola');
-	res.send('pong');
-});
+router.get('/', apiKeyMiddleware, barerTokenMiddleware, getUsers);
 
-router.post('/', (req, res) => {});
+router.post('/', apiKeyMiddleware, barerTokenMiddleware, createUser);
