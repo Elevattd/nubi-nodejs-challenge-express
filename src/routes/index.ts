1e8eb9fc36129951a07bc77fec2ef706be9f19cb
Elevattd <marianoo.14.md@gmail.com>
2023-06-20 03:51:08 -0300

    creo un condicional para desarollo y otro para prod TS>JS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,31 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 
 export const router = express.Router();
 
 const pathRouter = __dirname;
-const removeExtension = (fileName: string): string => fileName.split('.').shift()!;
+
+const removeExtension = (fileName: string): string => {
+	const extension = path.extname(fileName);
+	return fileName.replace(extension, '');
+};
 
 fs.readdirSync(pathRouter).filter(async (file) => {
-	const fileWWithOutExt = removeExtension(file);
-	let skip: boolean;
-	fileWWithOutExt ? (skip = ['index'].includes(fileWWithOutExt)) : (skip = false);
+	const fileWithOutExt = removeExtension(file);
+	const skip = ['index'].includes(fileWithOutExt);
+
 	if (!skip) {
-		const module = await import(`./${fileWWithOutExt}.ts`);
-		router.use(`/${fileWWithOutExt}`, module.router);
-		console.log('LOAD ROUTE     -->', fileWWithOutExt.toUpperCase());
+		let module;
+		if (file.endsWith('.js')) {
+			module = require(`./${fileWithOutExt}.js`);
+		} else if (file.endsWith('.ts')) {
+			module = await import(`./${fileWithOutExt}.ts`);
+		}
+
+		if (module && module.router) {
+			router.use(`/${fileWithOutExt}`, module.router);
+			console.log('LOAD ROUTE     -->', fileWithOutExt.toUpperCase());
+		}
 	}
 });
