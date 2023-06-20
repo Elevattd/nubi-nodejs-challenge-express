dc8d422c256b06875462348763cf413fdba6e493
Elevattd <marianoo.14.md@gmail.com>
2023-06-20 02:27:59 -0300

    creo enrutador de EP

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import fs from 'fs';
+
+export const router = express.Router();
+
+const pathRouter = __dirname;
+const removeExtension = (fileName: string): string => fileName.split('.').shift()!;
+
+fs.readdirSync(pathRouter).filter(async (file) => {
+	const fileWWithOutExt = removeExtension(file);
+	let skip: boolean;
+	fileWWithOutExt ? (skip = ['index'].includes(fileWWithOutExt)) : (skip = false);
+	if (!skip) {
+		console.log('🚀 👍 ~ fs.readdirSync ~ fileWWithOutExt:', fileWWithOutExt);
+		const module = await import(`./${fileWWithOutExt}.ts`);
+		router.use(`/${fileWWithOutExt}`, module.router);
+		console.log('LOAD ROUTE   -->', fileWWithOutExt.toUpperCase());
+	}
+});
