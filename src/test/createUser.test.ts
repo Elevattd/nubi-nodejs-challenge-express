810747061ae0eed64be06189fca8435ca7a6ac95
Elevattd <marianoo.14.md@gmail.com>
2023-06-20 05:13:56 -0300

    test unitario para createUser

---
+++ src/test/createUser.test.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+import { createUser } from '../services/users/createUsers';
+
+describe('createUser', () => {
+	let req: Request;
+	let res: Response;
+
+	beforeEach(() => {
+		req = {
+			body: {
+				// Mock the request body with valid user data
+				email: 'test@example.com',
+				name: 'John',
+				last_name: 'Doe',
+				sex_type: 'Male',
+				dni: 12345678,
+				birth_date: '1990-01-01',
+			},
+		} as Request;
+
+		res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+		} as any as Response;
+	});
+
+	it('should create a new user and return success message', async () => {
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(200);
+		expect(res.send).toHaveBeenCalledWith({
+			message: 'Usuario creado con éxito.',
+			data: expect.objectContaining({
+				wallet_id: expect.any(String),
+				created_at: expect.any(String),
+				email: 'test@example.com',
+				name: 'John',
+				last_name: 'Doe',
+				sex_type: 'Male',
+				dni: 12345678,
+				birth_date: '1990-01-01',
+			}),
+		});
+	});
+
+	it('should return 500 if an error occurs during user creation', async () => {
+		// Modify the req.body to trigger an error during user creation
+		req.body = null;
+
+		await createUser(req, res);
+
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.send).toHaveBeenCalledWith({
+			message: 'Hubo un error al crear al usuario.',
+			data: null,
+		});
+	});
+});
